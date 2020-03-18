6349299f835d5fa8f6ba6a02bfb80b3b1a24a49f
ssingh130 <ssingh30@syr.edu>
2020-03-17 20:07:30 -0400

    Added Hough circle detection through live video

---
+++ HoughCricle.cpp
@@ -0,0 +1,50 @@
+#include<opencv.hpp>
+#include<opencv_modules.hpp>
+#include<iostream>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	VideoCapture cap(0);
+
+	if (!cap.isOpened()) {
+		cout << "Error opening video stream or file" << endl;
+		return -1;
+	}
+	while (1) {
+		Mat frame;
+		//capture frame by frame
+		cap >> frame;
+		//if frame is empty break immediately
+		if (frame.empty())
+			break;
+		imshow("frame", frame);
+
+		Mat gray_frame;
+		cvtColor(frame, gray_frame, COLOR_BGR2GRAY);
+		imshow("gray", gray_frame);
+
+		GaussianBlur(gray_frame, gray_frame, Size(9, 9), 2, 2);
+		vector<Vec3f>circles;
+		HoughCircles(gray_frame, circles, HOUGH_GRADIENT, 1, gray_frame.rows / 8, 200, 50, 0, 0);
+
+		//Drawing Circles
+		for (size_t i = 0; i < circles.size(); i++) {
+			Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+			int radius = cvRound(circles[i][2]);
+			//circle center
+			circle(frame, center, 3, Scalar(0, 0, 255), -1, 8, 0);
+			//outer circle
+			circle(frame, center, radius, Scalar(0, 0, 255), 3, 8, 0);
+		}
+		imshow("HCT", frame);
+		// Press  ESC on keyboard to exit
+		char c = (char)waitKey(25);
+		if (c == 27)
+			break;
+	}
+	cap.release();
+	destroyAllWindows();
+	return 0;
+}
