e132d59ddb52cc6514a48d968ff75ca50ccccabf
ssingh130 <ssingh30@syr.edu>
2020-03-28 15:56:09 -0400

    added video capture

---
+++ ArucoAR.cpp
@@ -0,0 +1,54 @@
+#include<aruco.hpp>
+#include<opencv.hpp>
+#include<iostream>
+#include"aruco/dictionary.hpp"
+
+
+using namespace std;
+using namespace cv;
+using namespace aruco;
+
+int DetectMarker(Mat& source) {
+
+
+	Ptr <Dictionary> dictionary = getPredefinedDictionary(DICT_6X6_250);
+	Ptr<DetectorParameters>parameters = DetectorParameters::create();
+
+	//vectors that will contain the detected and the rejected markers
+	vector<vector<Point2f>> markerCorners, rejectedCandidates;
+
+	vector<int>markerIds;
+	detectMarkers(source,dictionary,markerCorners,markerIds,parameters,rejectedCandidates);
+	return 0;
+}
+
+int main(int argv, char** argc) {
+	VideoCapture cap(0);
+
+	if (!cap.isOpened()) {
+		cout << "Error opening video stream or file" << endl;
+		return -1;
+	}
+	while (1) {
+		Mat frame;
+		//capture frame by frame
+		cap >> frame;
+		//if frame is empty break immediately
+		if (frame.empty())
+			break;
+		imshow("frame", frame);
+
+		Mat markerImg;
+		Ptr <Dictionary> dictionary = getPredefinedDictionary(DICT_6X6_250);
+
+		aruco::drawMarker(dictionary, 33, 200, markerImg, 1);
+		// Press  ESC on keyboard to exit
+		char c = (char)waitKey(25);
+		if (c == 27)
+			break;
+	}
+	Mat frame;
+	DetectMarker(frame);
+	cap.release();
+	destroyAllWindows();
+}
